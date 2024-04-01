b308a5de196a571b6f0a012ceea3f8ff7ee140fd
Mikołaj Urbański <m.urbanski@goldensubmarine.com>
2024-04-01 15:10:41 +0200

    Create storybooks for 3 states of fetching using MSW

---
+++ src/data.ts
@@ -0,0 +1,64 @@
+export const mockData = {
+    "items": [
+        {
+            "count": 286439,
+            "name": "excel"
+        },
+        {
+            "count": 150,
+            "name": "word"
+        },
+        {
+            "count": 500,
+            "name": "powerpoint"
+        },
+        {
+            "count": 789,
+            "name": "outlook"
+        },
+        {
+            "count": 2000,
+            "name": "chrome"
+        },
+        {
+            "count": 120,
+            "name": "firefox"
+        },
+        {
+            "count": 350,
+            "name": "safari"
+        },
+        {
+            "count": 600,
+            "name": "photoshop"
+        },
+        {
+            "count": 250,
+            "name": "illustrator"
+        },
+        {
+            "count": 100,
+            "name": "premiere"
+        },
+        {
+            "count": 450,
+            "name": "aftereffects"
+        },
+        {
+            "count": 800,
+            "name": "spotify"
+        },
+        {
+            "count": 670,
+            "name": "apple music"
+        },
+        {
+            "count": 300,
+            "name": "netflix"
+        },
+        {
+            "count": 1500,
+            "name": "youtube"
+        }
+    ],
+};
--- src/stories/TagsTable.stories.tsx
+++ src/stories/TagsTable.stories.tsx
@@ -3,6 +3,10 @@ import "../index.css";
 import TagsTable from "../components/TagsTable/TagsTable.tsx";
 import {Provider} from "react-redux";
 import {store} from "../store/store.ts";
+import { http, HttpResponse, delay } from 'msw';
+import {useEffect} from "react";
+import {mockData} from "../data.ts";
+
 
 const meta = {
     title: 'TagsTable',
@@ -17,9 +21,77 @@ const meta = {
             </Provider>
         ),
     ],
+
 } satisfies Meta<typeof TagsTable>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+export const Success: Story = {};
+
+Success.decorators = [
+    (Story) => {
+        useEffect(() => {
+            return () => window.location.reload();
+        }, []);
+
+        return <Story />;
+    },
+];
+
+Success.parameters = {
+    msw: {
+        handlers: [
+            http.get('https://api.stackexchange.com/2.3/tags?site=stackoverflow', () => {
+                return HttpResponse.json(mockData);
+            }),
+        ],
+    },
+};
+
+export const Loading: Story = {};
+
+Loading.decorators = [
+    (Story) => {
+        useEffect(() => {
+            return () => window.location.reload();
+        }, []);
+
+        return <Story />;
+    },
+];
+
+Loading.parameters = {
+    msw: {
+        handlers: [
+            http.get('https://api.stackexchange.com/2.3/tags?site=stackoverflow', async () => {
+                await delay(5000);
+                return HttpResponse.json(mockData);
+            }),
+        ],
+    },
+};
+
+export const Error: Story = {};
+
+Error.decorators = [
+    (Story) => {
+        useEffect(() => {
+            return () => window.location.reload();
+        }, []);
+
+        return <Story />;
+    },
+];
+
+Error.parameters = {
+    msw: {
+        handlers: [
+            http.get('https://api.stackexchange.com/2.3/tags?site=stackoverflow', () => {
+                return new HttpResponse(null, {
+                    status: 500,
+                })
+            }),
+        ],
+    },
+};
